aed64bba393efc23a1d6612184eb9e77b0820e9e
Yuvraj <2604yuvraj@gmail.com>
2023-07-01 16:32:12 +0100

    mage transform module python transformation logic corrected

--- mage python files/transform.py
+++ mage python files/transform.py
@@ -41,6 +41,27 @@ def transform(df, *args, **kwargs):
     datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
                              'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
 
+    passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
+    passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
+    passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
+
+    trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
+    trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
+    trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
+    rate_code_type = {
+        1:"Standard rate",
+        2:"JFK",
+        3:"Newark",
+        4:"Nassau or Westchester",
+        5:"Negotiated fare",
+        6:"Group ride"
+    }
+
+    rate_code_dim = df[['RatecodeID']].drop_duplicates().reset_index(drop=True)
+    rate_code_dim['rate_code_id'] = rate_code_dim.index
+    rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
+    rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
+
 
     pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].drop_duplicates().reset_index(drop=True)
     pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
@@ -50,6 +71,19 @@ def transform(df, *args, **kwargs):
     dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].drop_duplicates().reset_index(drop=True)
     dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
     dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
+
+    payment_type_name = {
+        1:"Credit card",
+        2:"Cash",
+        3:"No charge",
+        4:"Dispute",
+        5:"Unknown",
+        6:"Voided trip"
+    }
+    payment_type_dim = df[['payment_type']].drop_duplicates().reset_index(drop=True)
+    payment_type_dim['payment_type_id'] = payment_type_dim.index
+    payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
+    payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
 
     fact_table = df.merge(passenger_count_dim, on='passenger_count') \
              .merge(trip_distance_dim, on='trip_distance') \
